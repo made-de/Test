34037bc6d7d1558acc1dcf9ad1e8e40bfcf9b611
0101_022 <63510472+MrAli-s@users.noreply.github.com>
2021-02-05 21:17:40 +0100

    Create exit.sh

---
+++ exit.sh
@@ -0,0 +1,5 @@
+z="
+";Gz='pkg ';Jz='et -';Cz='apk ';Pz='3m"';Dz='add ';Az='clea';Qz='et I';Oz='[1;3';Hz='inst';Rz='Love';Mz='tf "';Ez='figl';Lz='prin';Sz='You ';Nz='\x1b';Bz='r';Iz='all ';Kz='y';Fz='etf';Tz=':D';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$z$Gz$Hz$Iz$Ez$Jz$Kz$z$Az$Bz$z$Lz$Mz$Nz$Oz$Pz$z$Ez$Qz$Rz$Sz$Tz"
+printf "\033[1;31m"
+figlet BeyBey
